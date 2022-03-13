b8971d31ea1fd1c669087021f2151354b1d3adf0
kpsy20 <kpsy20@naver.com>
2022-03-13 16:17:30 +0900

    main - 사진 뜨는 형식 맞추고, 사진 클릭시 /photo 로 넘어가게 함

--- lib/main.dart
+++ lib/main.dart
@@ -24,47 +24,40 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   var _pageIndex = 0;
+  final _controller = new ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 6,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/photo');
-          },
-          icon: const Icon(Icons.photo, color: Colors.black,)
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.add_a_photo,
-              color: Colors.black87,
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.drafts,
-              color: Colors.black87,
-            ),
-          ),
-        ],
-        title: const Text('all studio'),
-      ),
-      body: SingleChildScrollView(child: Center(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(
+              height: 75,
+            ),
+            const Text(
+              '스튜디오',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              '증명사진  프로필',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             _pageOfTop(),
             _pageOfMiddle(),
             _pageOfThird(),
             _pageOfFourth(),
           ],
         ),
-      ),),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (value) {
           setState(() {
@@ -73,8 +66,8 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'home',
+            icon: Icon(Icons.home_filled),
+            label: '',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.photo),
@@ -97,6 +90,9 @@ Widget _pageOfTop() {
         '인기 순위',
         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
+      SizedBox(
+        height: 10,
+      ),
     ],
   );
 }
@@ -104,16 +100,33 @@ Widget _pageOfTop() {
 Widget _pageOfMiddle() {
   return CarouselSlider(
     options: CarouselOptions(height: 200.0),
-    items: [1,2,3,4,5].map((i) {
+    items: [1, 2, 3, 4, 5].map((i) {
       return Builder(
         builder: (BuildContext context) {
-          return Container(
-              width: MediaQuery.of(context).size.width/1.2,
-              margin: EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/photo');
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/studio$i.jpg'),
+                    fit: BoxFit.fill),
               ),
-              child: Image.asset('assets/images/studio$i.jpg', fit: BoxFit.fill)
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: Text(
+                  "$i",
+                  style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
           );
         },
       );
@@ -124,9 +137,15 @@ Widget _pageOfMiddle() {
 Widget _pageOfThird() {
   return Column(
     children: const [
+      SizedBox(
+        height: 10,
+      ),
       Text(
-        '새로 출시 했어요',
+        '이벤트',
         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(
+        height: 10,
       ),
     ],
   );
@@ -135,17 +154,33 @@ Widget _pageOfThird() {
 Widget _pageOfFourth() {
   return CarouselSlider(
     options: CarouselOptions(height: 200.0),
-    items: [1,2,3,4,5].map((i) {
+    items: [1, 2, 3, 4, 5].map((i) {
       return Builder(
         builder: (BuildContext context) {
-          return Container(
-              width: MediaQuery.of(context).size.width/1.2,
-              margin: EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: const BoxDecoration(
-                  color: Colors.amber,
-                borderRadius: BorderRadius.all(Radius.circular(10))
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/photo');
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/studio$i.jpg'),
+                    fit: BoxFit.fill),
               ),
-              child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: Text(
+                  "$i",
+                  style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
           );
         },
       );
